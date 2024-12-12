e51d34eb405134d87822f0921ed84096f1b6f3a8
tim <timothykbook@gmail.com>
2024-12-12 12:15:15 -0500

    day 7

--- 07-day/main.py
+++ 07-day/main.py
@@ -1,11 +1,13 @@
-from itertools import permutations
+from itertools import product
 from collections import deque
 
-file = 'sample.txt'
+#  file = 'sample.txt'
+file = 'input.txt'
 raw = open(file, 'r').readlines()
 
 add = lambda a, b: a + b
 mul = lambda a, b: a * b
+cat = lambda a, b: int(str(a) + str(b))
 
 class Equation:
     def __init__(self, line):
@@ -19,21 +21,47 @@ class Equation:
     def count_ops(self):
         n_ops = len(self.nums) - 1
         count = 0
-        import pdb; pdb.set_trace()
-        for op_list in permutations([add, mul], n_ops):
-            ans = self.nums[0]
-            import pdb; pdb.set_trace()
-            for op, b in zip(op_list, self.nums[1:]):
-                print(f"Op({ans}, {b}) -> {op(ans, b)}")
-                ans = op(ans, b)
+        for ops in product(*['*+' for _ in range(n_ops)]):
+            ops = deque(ops)
+            vec = deque(self.nums.copy())
+            while ops:
+                #  print(f"vec = {vec}")
+                op = {'*': mul, '+': add}[ops.popleft()]
+                a = vec.popleft()
+                b = vec.popleft()
+                vec.appendleft(op(a, b))
 
-            if ans == self.res:
+            if vec[0] == self.res:
                 count += 1
+
         return count
-            
+
+    def count_ops_b(self):
+        n_ops = len(self.nums) - 1
+        count = 0
+        for ops in product(*['*+|' for _ in range(n_ops)]):
+            ops = deque(ops)
+            vec = deque(self.nums.copy())
+            while ops:
+                #  print(f"vec = {vec}")
+                op = {'*': mul, '+': add, '|': cat}[ops.popleft()]
+                a = vec.popleft()
+                b = vec.popleft()
+                vec.appendleft(op(a, b))
+
+            if vec[0] == self.res:
+                count += 1
+
+        return count
+
             
 
 eqns = [Equation(line) for line in raw]
-#  sols = [eqn.res for eqn in eqns if eqn.count_ops() > 0]
-#  sol = sum(sols)
-#  print(f"A ::: {sol}")
+sols = [eqn.res for eqn in eqns if eqn.count_ops() > 0]
+sol = sum(sols)
+print(f"A ::: {sol}")
+
+eqns = [Equation(line) for line in raw]
+sols = [eqn.res for eqn in eqns if eqn.count_ops_b() > 0]
+sol = sum(sols)
+print(f"B ::: {sol}")
